78d844c0476634e629d6953a9199c98c73ee44fc
Noka993 <Noka993@wp.pl>
2025-04-02 16:27:23 +0200

    Initialized db migrations

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from backend.core.database import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
 
 db.init_app(app)
+
+migrate = Migrate(app, db)
